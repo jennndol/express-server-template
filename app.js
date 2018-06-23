6f08b7ba21b7c5e3b298b876b6ca51ac51926c0b
Luthfi <vlootfie@gmail.com>
2018-06-23 16:33:52 +0700

    add endpoint test example using mocha chai

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ app.use(cors());
 app.use(json());
 app.use(urlencoded({ extended: false }));
 
+app.get('/', (req, res) => res.status(200).json({code: 200, message: 'Hello world'}));
+
 app.listen(process.env.PORT, () => {
 	Console.log('The system is run on port', process.env.PORT);
 });
+
+export default app;
--- test/test.js
+++ test/test.js
@@ -1,8 +1,22 @@
-import { assert } from 'chai';
+import { assert, request, use } from 'chai';
+import chaiHttp from 'chai-http';
+import app from '../app';
 
-describe('Testing', function() {
-	it('should return true', function(done) {
+use(chaiHttp);
+
+describe('Testing', () => {
+	it('should return true', (done) => {
 		assert.equal(1, 1);
 		done();
+	});
+
+	it('should get Hello world message', () => {
+		request(app)
+			.get('/')
+			.end((e, res) => {
+				assert.equal(res.status, 200);
+				assert.equal(res.body.code, 200);
+				assert.equal(res.body.message, 'Hello world');
+			});
 	});
 });
